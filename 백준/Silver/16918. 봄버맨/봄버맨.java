cd780691dcf7368aa697f8817af8273b92a8f4d4
godjuhyuk <59328108+godjuhyuk@users.noreply.github.com>
2023-07-27 19:54:24 +0900

    [Silver I] Title: 봄버맨, Time: 296 ms, Memory: 62984 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.java
+++ 백준/Silver/16918. 봄버맨/봄버맨.java
@@ -5,7 +5,21 @@ public class Main {
 	static int n, m, k, sec, qSize;
 	static int[][] deltaBomb = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 	static int[][] boomTimeTable;
-	static Queue<int[]> bfsQueue;
+	static List<int[]> boomList;
+	
+	
+	public static void boom(int r, int c) {
+		for(int[] delta: deltaBomb) {
+			int nr = r + delta[0];
+			int nc = c + delta[1];
+			// 이번 큐에 터질 애들이면 continue
+            if(boomTimeTable[nr][nc]==1) {
+				continue;
+			}
+			boomTimeTable[nr][nc] = 0;
+		}	
+	}
+	
 	public static void setBomb() {
 		
 		sec++;
@@ -13,7 +27,7 @@ public class Main {
 			for(int j=1; j<=m; j++) {
 				if(boomTimeTable[i][j] == 2) {
 					boomTimeTable[i][j] -= 1;
-					bfsQueue.add(new int[] {i, j});
+					boomList.add(new int[] {i, j});
 				}
 				else if(boomTimeTable[i][j] == 0) {
 					boomTimeTable[i][j] += 2;
@@ -24,23 +38,15 @@ public class Main {
 	}
 	
 	
-	public static void bfsBoom() {
+	public static void dfsBoom() {
 		sec++;
-		for(int i=0; i<qSize; i++) {
-			int[] temp = bfsQueue.poll(); //1초 남은 애들이 0 되면서 터질 준비
-			int r = temp[0];
-			int c = temp[1];
+		for(int[] loc: boomList) {
+			int r = loc[0];
+			int c = loc[1];
 			boomTimeTable[r][c] = 0;
-			for(int[] delta: deltaBomb) {
-				int nr = r + delta[0];
-				int nc = c + delta[1];
-				// 이번 큐에 터질 애들이면 continue
-                if(boomTimeTable[nr][nc]==1) {
-					continue;
-				}
-				boomTimeTable[nr][nc] = 0;
-			}	
+			boom(r, c);
 		}
+		boomList.clear();
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -51,7 +57,7 @@ public class Main {
 		k = Integer.parseInt(input[2]);
 		char[][] inputMap = new char[n][m];
 		boomTimeTable = new int[n+2][m+2];
-		bfsQueue = new LinkedList();
+		boomList = new ArrayList<>();
 		
 		sec++; // 초기 폭탄 설치 
 		for(int i=1; i<=n; i++) {
@@ -67,8 +73,7 @@ public class Main {
 			if(sec >= k) {
 				break;
 			}
-			qSize = bfsQueue.size();
-			bfsBoom();
+			dfsBoom();
 		}
 		
 		StringBuilder sb = new StringBuilder();
