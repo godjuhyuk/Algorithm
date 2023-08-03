a448014032b89981143ae79a0ee9f773d3ff9087
godjuhyuk <59328108+godjuhyuk@users.noreply.github.com>
2023-08-03 16:15:04 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 302 ms, Memory: 19,804 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -1,100 +1,95 @@
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+	import java.io.IOException;
+	import java.io.InputStreamReader;
+	import java.util.Arrays;
 
-public class Solution {
-	
-//	10
-//	5
-//	0 0 100 100 70 40 30 10 10 5 90 70 50 20
-//	6
-//	88 81 85 80 19 22 31 15 27 29 30 10 20 26 5 14
-//	10
-//	39 9 97 61 35 93 62 64 96 39 36 36 9 59 59 96 61 7 64 43 43 58 1 36
-//	...
-	private static int n, ans;
-	private static int[] combList;
-	private static boolean[] isSelected;
-	private static Location[] customerInfo;
-	private static Location homeInfo, compInfo;
-	
-	private static int calculateDist(Location a, Location b) {
-		return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
-	}
-	
-	
-	public static int getDistance() {
-		int distSum = calculateDist(compInfo, customerInfo[combList[0]]);
-		for(int i=0; i<n-1; i++) {
-			distSum += calculateDist(customerInfo[combList[i]], customerInfo[combList[i+1]]);
+	public class Solution {
+		
+		private static int n, ans;
+		private static int[] combList;
+		private static boolean[] isSelected;
+		private static Location[] customerInfo;
+		private static Location homeInfo, compInfo;
+		
+		// 거리 계산
+		private static int calculateDist(Location a, Location b) {
+			return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
 		}
 		
-		distSum += calculateDist(customerInfo[combList[n-1]], homeInfo);
-		return distSum;
-	}
-	
-	
-	private static  void perm(int cnt) {
-		if(cnt == n) {
-			ans = Math.min(ans, getDistance());
-			return;
+		
+//		public static int getDistance() {
+//			// 거리 합 계산
+//			int distSum = calculateDist(compInfo, customerInfo[combList[0]]);
+//			for(int i=0; i<n-1; i++) {
+//				if(distSum > ans) {
+//					return ans+1;
+//				}
+//				distSum += calculateDist(customerInfo[combList[i]], customerInfo[combList[i+1]]);
+//			}
+//			distSum += calculateDist(customerInfo[combList[n-1]], homeInfo);
+//			return distSum;
+//		}
+		
+		
+		private static  void perm(int cnt, int sum, Location lastLoc) {
+			if(cnt == n) {
+				sum += calculateDist(lastLoc, homeInfo);
+				ans = Math.min(ans, sum);
+				return;
+			}
+			
+			if(ans != Integer.MAX_VALUE && sum >= ans) {
+				return;
+			}
+			
+			for(int i=0; i<n; i++) {
+				if(!isSelected[i]) {
+					// i+2인 이유 : 0, 1 인덱스에는 집과 회사가 있어서
+					isSelected[i] = true;
+					perm(cnt+1, sum+calculateDist(customerInfo[i+2], lastLoc), customerInfo[i+2]);
+					isSelected[i] = false;
+				}
+			}
 		}
 		
-		for(int i=0; i<n; i++) {
-			if(!isSelected[i]) {
-				isSelected[i] = true;
-				combList[cnt] = i+2;
-				perm(cnt+1);
-				isSelected[i] = false;
+		public static void main(String[] args) throws IOException
+		{
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			StringBuilder sb = new StringBuilder();
+			int T = Integer.parseInt(br.readLine());
+			
+			for(int testCase = 1; testCase <= T; testCase++) {
+				ans = Integer.MAX_VALUE;
+				n = Integer.parseInt(br.readLine());
+				combList = new int[n];
+				isSelected = new boolean[n];
+				customerInfo = new Location[n+2];
+				String[] input = br.readLine().split(" ");
+				
+				// Location 정보 등록
+				for(int i=0; i<input.length; i+=2) {
+					customerInfo[i/2] = new Location(Integer.parseInt(input[i]), Integer.parseInt(input[i+1]));
+				}
+				
+				compInfo = customerInfo[0];
+				homeInfo = customerInfo[1];
+				
+				perm(0, 0, compInfo);
+				
+				sb.append("#").append(testCase).append(' ').append(ans).append('\n');
+			}
+			System.out.println(sb);
+		}
+		
+			
+			
+		public static class Location {
+			int x;
+			int y;
+			
+			Location(int x, int y) {
+				this.x = x;
+				this.y = y;
 			}
 		}
 	}
-	
-	public static void main(String[] args) throws IOException
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int testCase = 1; testCase <= T; testCase++) {
-			ans = Integer.MAX_VALUE;
-			n = Integer.parseInt(br.readLine());
-			combList = new int[n];
-			isSelected = new boolean[n];
-			customerInfo = new Location[n+2];
-			String[] input = br.readLine().split(" ");
-			for(int i=0; i<input.length; i+=2) {
-				customerInfo[i/2] = new Location(Integer.parseInt(input[i]), Integer.parseInt(input[i+1]));
-			}
-			compInfo = customerInfo[0];
-			homeInfo = customerInfo[1];
-			
-			perm(0);
-			
-			sb.append("#").append(testCase).append(' ').append(ans).append('\n');
-		}
-		System.out.println(sb);
-	}
-	
-		
-		
-	public static class Location {
-		int x;
-		int y;
-		
-		Location(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-
-		public int getX() {
-			return x;
-		}
-
-		public int getY() {
-			return y;
-		}
-		
-	}
-}
