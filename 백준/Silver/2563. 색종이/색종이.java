3ae9eb249a38c9e47996229c1cde437f3ca4a3ca
godjuhyuk <59328108+godjuhyuk@users.noreply.github.com>
2023-08-08 16:00:29 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -2,14 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.nextLine());
-        int sum=0;
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+        int n = Integer.parseInt(br.readLine());
+        int x = 0;
+        int y = 0;
+        int sum = 0;
         boolean[][] grid = new boolean[101][101];
         for(int i=0; i<n; i++){
-            int x = sc.nextInt();
-            int y = sc.nextInt();
+        	String[] input = br.readLine().split(" ");
+        	
+            x = Integer.parseInt(input[0]);
+            y = Integer.parseInt(input[1]);
+            
             for(int a=x; a<x+10; a++){
                 for(int b=y; b<y+10; b++){
                     if(!grid[a][b]){
