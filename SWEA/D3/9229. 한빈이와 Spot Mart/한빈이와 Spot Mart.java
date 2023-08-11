0a2f57b91fcf15573057f8c1fdd31c08f5afdef9
godjuhyuk <59328108+godjuhyuk@users.noreply.github.com>
2023-08-11 11:44:33 +0900

    [D3] Title: 한빈이와 Spot Mart, Time: 763 ms, Memory: 114,644 KB -BaekjoonHub

--- SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
+++ SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
@@ -25,10 +25,8 @@
 //System.out.println(var);		       				   // 문자열 1개 출력하는 예제
 //System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
 /////////////////////////////////////////////////////////////////////////////////////////////
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 //import java.io.FileInputStream;
 
 /*
@@ -38,102 +36,55 @@ import java.util.Arrays;
 class Solution
 {
 
-
-/**
- *
- * 
- * N개의 과자 중 2개를 뽑아서 무게 상한선 내에서 최댓값을 구하는 문제이다.
- * 
- * 완탐 vs 조합 => 뭐가 빠를까?
- * 
- * 입력받을때 M이상은 제외
- * 
- * 다른 사람이 조합으로 풀테니 난 완탐으로 풀어보자.
- * 중간에 딱 M이 맞춰지면 중단하는 식으로 하면 되겠다. 
- * 정렬이 도움이 될까? => 도움이 될것같다.
- * 정렬 후 완탐으로 도중에 continue 하는 식으로 해보자.
- * 
- * 
- * 예상 시간 복잡도 : O(N^2)
- * @author SSAFY
- */
-//	4
-//	3 45
-//	20 20 20
-//	6 10
-//	1 2 5 8 9 11
-//	4 100
-//	80 80 60 60
-//	4 20
-//	10 5 10 16	// 테스트 케이스 개수
-	// 첫 번째 테스트 케이스 N = 3, M = 45
-	// 3개의 과자 봉지 무게 a1 = 20, a2 = 20, a3 = 20
-
-	private static int N, M;
-	private static StringBuilder sb;
-	
-	private static int solve(int start, int[] snacks, int maxWeight) {
-		// TODO Auto-generated method stub
-		
-		for(int i= start; i<N-1; i++) {
-			for(int j=i+1; j<N; j++) {
-				if(snacks[i] + snacks[j] <= M) {
-					maxWeight = Math.max(maxWeight, snacks[i] + snacks[j]);
-				}
-				
-				if(maxWeight == M) {
-					return M;
-				}
-			}
-		}
-		
-		
-		return maxWeight > 0 ? maxWeight : -1;
-		
-	}
-	
-	public static void main(String[] args) throws IOException
-	{
-		sb = new StringBuilder();
-		
+public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int testCase = 1; testCase <= T; testCase++) {
-			int maxWeight = 0;
-			String[] input = br.readLine().split(" ");
-			int zeroCnt = 0;
-			N = Integer.parseInt(input[0]);
-			M = Integer.parseInt(input[1]);
-			
-			int[] snacks = new int[N];
-			
-			input = br.readLine().split(" ");
+		int testCase = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		for(int t=1; t<=testCase; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			int cnt =0;
+			int answer = -1;
+			int[] visited = new int[M];
+			st = new StringTokenizer(br.readLine());
 			for(int i=0; i<N; i++) {
-				
-				int snackWeight = Integer.parseInt(input[i]);
-				if(snackWeight >= M) {
-					zeroCnt++;
-					continue;
+				int weight = Integer.parseInt(st.nextToken());
+				if(weight < M) {
+					cnt++;
+					visited[weight]++;
+				};
+			};
+
+			int[] snack = new int[cnt];
+			for(int i=1, j=0; i<M; i++) {
+				if(visited[i]>0) {
+					while(visited[i] >0) {
+						snack[j++] = i;
+						visited[i]--;
+					}
 				}
-				
-				snacks[i] = snackWeight;
 			}
-			
-			Arrays.sort(snacks);
-			
-			if(zeroCnt == 0) {
-				maxWeight = solve(0, snacks, maxWeight);
-			} else {
-				maxWeight = solve(zeroCnt, snacks, maxWeight);
+
+			int start = 0;
+			int end = cnt-1;
+			while(start < end) {
+				if(snack[start] + snack[end] > M) {
+					end -= 1;
+				}
+				else if(snack[start] + snack[end] < M) {
+					answer = Math.max(answer, snack[start] + snack[end]);
+					start += 1;
+				}
+				else {
+					answer = M;
+					break;
+				}
 			}
-			
-			sb.append('#').append(testCase).append(' ').append(maxWeight).append('\n');
+			sb.append('#').append(t).append(' ').append(answer).append('\n');
 		}
-		
 		System.out.println(sb);
-		
 	}
 	 
 }
