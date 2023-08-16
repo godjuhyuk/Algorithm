9c211137d40e786c833d3646f0de713966fff89d
godjuhyuk <59328108+godjuhyuk@users.noreply.github.com>
2023-08-16 17:04:52 +0900

    [Silver I] Title: 징검다리 건너기, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/21317. 징검다리 건너기/징검다리 건너기.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 
+ * 1번 돌부터 N-1돌까지 각 점프 당 필요한 에너지양이 다음과 같이 주어진다.
+ * [작은 점프, 큰 점프]
+ * 
+ * K는 딱 한번 엄청 큰 점프를 할 수 있는 에너지 필요량이다.
+ * 
+ * 점화식을 구해보자.
+ * 
+ * dp[x] : x번째돌까지 뛰었을때의 최소 필요량
+ * 
+ * K가 한번 쓰였다는걸 어떻게 반영해야할까? 
+ * => 1번째 idx에 boolean을 넣어볼까?
+ * 
+ * 초기값 : dp[1] = 0, dp[2] = jump[1][0]
+ * i >= 3 에 대해,
+ * dp[i] = Math.min(dp[i],
+ * 					dp[i-1] + jump[i-1][0],
+ * 					dp[i-2] + jump[i-2][1],
+ * 					dp[i-3] + K)
+ * 					 
+ * 
+ * @author SSAFY
+ *
+ */
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int[][] jump = new int[N+1][2];
+		int[][] dp = new int[N+1][2];
+		
+		for(int i=1; i<=N-1; i++) {
+			st = new StringTokenizer(br.readLine());
+			jump[i][0] = Integer.parseInt(st.nextToken());
+			jump[i][1] = Integer.parseInt(st.nextToken());
+			
+			dp[i][0] = dp[i-1][0] + jump[i-1][0];
+		}
+		
+		dp[N][0] = dp[N-1][0] + jump[N-1][0];
+		
+		st = new StringTokenizer(br.readLine());
+		int K = Integer.parseInt(st.nextToken());
+		
+		if(N==2) {
+			System.out.println(jump[1][0]);
+			return;
+		} 
+		if(N==3) {
+			System.out.println(Math.min(jump[1][1], dp[2][0] +  jump[2][0]));
+			return;
+		}
+		if(N>=4) {
+			dp[3][0] = Math.min(jump[1][1], dp[2][0] +  jump[2][0]);
+			dp[4][0] = Math.min(dp[4][0], Math.min(dp[3][0] + jump[3][0], dp[2][0] + jump[2][1]));
+			dp[4][1] = K;
+		}
+		if(N>=5) {
+			dp[5][0] = Math.min(dp[5][0], Math.min(dp[4][0] + jump[4][0], dp[3][0] + jump[3][1]));
+			dp[5][1] = Math.min(dp[4][1] + jump[4][0], dp[2][0] + K);
+		}
+		
+		
+		for(int i=6; i<=N; i++) {
+			
+			dp[i][0] = Math.min(dp[i][0], Math.min(dp[i-1][0] + jump[i-1][0], dp[i-2][0] + jump[i-2][1]));
+			dp[i][1] = Math.min(dp[i-3][0] + K, Math.min(dp[i-1][1] + jump[i-1][0], dp[i-2][1] + jump[i-2][1]));
+			
+		}
+		
+		System.out.println(Math.min(dp[N][0], dp[N][1]));
+	}
+}
