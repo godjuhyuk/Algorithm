987aa089135a09bda7eb686069430a91b8551811
godjuhyuk <59328108+godjuhyuk@users.noreply.github.com>
2023-08-16 15:41:22 +0900

    [Gold V] Title: 암호 만들기, Time: 80 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,67 +1,81 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+/**
+ * 시작시간: 3시 12분
+ * 종료시간: 3시 19분
+ * 
+ * 출력조건
+ * a. 최소 1개의 모음, 2개의 자음으로 구성
+ * b. 오름차순 정렬된 상태로 출력
+ * 
+ * 풀이방법
+ * 
+ * 재귀를 이용한 순열 구현 (정렬을 해놓고 시작한다면 b를 만족시키기는 편함) => a만 만족하도록 하면 됨
+ * 
+ * 
+ * 구현해야 할것
+ * 1. 모음 cnt, 자음 cnt 하는 자료구조 and 메소드 구현
+ * => int로 처리
+ * 
+ * 2. 순열 재귀함수 구현
+ * => 순서 신경써서 start parameter를 넘겨주기
+ * 
+ * 3. 출력 => StringBuilder를 전역으로 선언해서 사용
+ *  
+ * @author SSAFY
+ *
+ */
 public class Main {
-	static int n, m, aCnt, bCnt;
-	static boolean[] visited;
-	static String[] strList;
-	static List<String> pwList;
-	static StringBuilder sb;
+	private static int L, C;
+	private static String[] candidates, combList;
+	private static StringBuilder sb;
 	
-	
-	public static void charCount(String str) {
-		if(str.equals("a") || str.equals("e") || str.equals("i") || str.equals("o") || str.equals("u")) {
-			aCnt++;
-		} else {
-			bCnt++;
+	private static void print() {
+		for(int i=0; i<L; i++) {
+			sb.append(combList[i]);
 		}
-		
+		sb.append('\n');
 	}
 	
-	public static void dfs(int start, int depths) {
-		if(depths==n) {
-			for(int i=0; i<n; i++) {
-				String temp = pwList.get(i);
-				charCount(temp);
+	private static void combi(int depths, int start, int mCnt, int jCnt) {
+		
+		if(depths == L) {
+			if(mCnt >= 1 && jCnt >= 2) {
+				print();
 			}
-			if(aCnt>=1 && bCnt>=2) {
-				for(int i=0; i<n; i++) {
-					sb.append(pwList.get(i));
-				}
-				sb.append("\n");
-			}
-			aCnt=0;
-			bCnt=0;
 			return;
 		}
-		for(int i=start; i<strList.length; i++) {
-			if(!visited[i]) {
-				visited[i] = true;
-				pwList.add(strList[i]);
-				dfs(i, depths+1);
-				visited[i] = false;
-				pwList.remove(strList[i]);
+		
+		for(int i=start; i<C; i++) {
+			combList[depths] = candidates[i];
+			char tempChar = candidates[i].charAt(0);
+			if(tempChar == 'a' || tempChar == 'e' || tempChar == 'i' || tempChar == 'o' || tempChar == 'u') {
+				combi(depths+1, i+1, mCnt+1, jCnt);
+			} else {
+				combi(depths+1, i+1, mCnt, jCnt+1);
 			}
 		}
-		
-		
+			
+
 	}
 	
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuilder();
-        
-        String[] input = br.readLine().split(" ");
-        n = Integer.parseInt(input[0]);
-        m = Integer.parseInt(input[1]);
-        
-        strList = br.readLine().split(" ");
-        Arrays.sort(strList);
-        pwList = new ArrayList();
-        visited = new boolean[m];
-        dfs(0, 0);
-    	System.out.println(sb.toString());
-    	
-        
-    }
+	public static void main(String[] args) throws IOException {
+		sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		L = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		
+		combList = new String[L];
+		candidates = br.readLine().split(" ");
+		Arrays.sort(candidates);
+		
+		combi(0, 0, 0, 0);
+		System.out.println(sb);
+	}
 }
