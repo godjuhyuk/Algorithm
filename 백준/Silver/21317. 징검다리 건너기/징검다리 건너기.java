7faf394b026400121786914269a07456017ae29c
godjuhyuk <59328108+godjuhyuk@users.noreply.github.com>
2023-08-16 17:26:53 +0900

    [Silver I] Title: 징검다리 건너기, Time: 80 ms, Memory: 11584 KB -BaekjoonHub

--- 백준/Silver/21317. 징검다리 건너기/징검다리 건너기.java
+++ 백준/Silver/21317. 징검다리 건너기/징검다리 건너기.java
@@ -9,12 +9,6 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int N = Integer.parseInt(st.nextToken());
-		
-		if(N==1) {
-			System.out.println(0);
-			return;
-		}
-		
 		int[][] jump = new int[N+1][2];
 		int[][] dp = new int[N+1][2];
 		
@@ -24,14 +18,12 @@ public class Main {
 			jump[i][1] = Integer.parseInt(st.nextToken());
 			
 			dp[i][0] = dp[i-1][0] + jump[i-1][0];
-			dp[i][1] = 5000 * 20;
 		}
 		
 		dp[N][0] = dp[N-1][0] + jump[N-1][0];
 		
 		st = new StringTokenizer(br.readLine());
 		int K = Integer.parseInt(st.nextToken());
-		
 		
 		if(N==2) {
 			System.out.println(jump[1][0]);
@@ -41,10 +33,18 @@ public class Main {
 			System.out.println(Math.min(jump[1][1], dp[2][0] +  jump[2][0]));
 			return;
 		}
+		if(N>=4) {
+			dp[3][0] = Math.min(jump[1][1], dp[2][0] +  jump[2][0]);
+			dp[4][0] = Math.min(dp[4][0], Math.min(dp[3][0] + jump[3][0], dp[2][0] + jump[2][1]));
+			dp[4][1] = K;
+		}
+		if(N>=5) {
+			dp[5][0] = Math.min(dp[5][0], Math.min(dp[4][0] + jump[4][0], dp[3][0] + jump[3][1]));
+			dp[5][1] = Math.min(dp[4][1] + jump[4][0], dp[2][0] + K);
+		}
 		
-		dp[3][0] = Math.min(jump[1][1], dp[2][0] +  jump[2][0]);
 		
-		for(int i=4; i<=N; i++) {
+		for(int i=6; i<=N; i++) {
 			
 			dp[i][0] = Math.min(dp[i][0], Math.min(dp[i-1][0] + jump[i-1][0], dp[i-2][0] + jump[i-2][1]));
 			dp[i][1] = Math.min(dp[i-3][0] + K, Math.min(dp[i-1][1] + jump[i-1][0], dp[i-2][1] + jump[i-2][1]));
