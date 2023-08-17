204dc48d19aa676727110eaaec535c227293f24a
godjuhyuk <59328108+godjuhyuk@users.noreply.github.com>
2023-08-17 17:09:15 +0900

    [Gold II] Title: 빵집, Time: 416 ms, Memory: 44444 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -3,39 +3,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
+
 public class Main {
 	private static int R, C, ans;
-	private static boolean map[][];
+	private static char map[][];
 	private static int deltas[][] = {{-1, 1}, {0, 1}, {1, 1}};
 	
-	private static boolean isInRange(int r, int c) {
-		return 0<=r && r< R && 0<=c && c<C;
-	}
-	
 	private static boolean findRoot(int r, int c) {
-		
-		if(c == C-1) {
+		if(c == C) {
 			ans++;
-			map[r][c] = false; // 파이프를 설치했으므로 false
-			return false;
+			map[r][c] = 'x';
+			return true;
 		}
 		
 		for(int[] d : deltas) {	
 			int nr = r + d[0];
 			int nc = c + d[1];
-			if(isInRange(nr, nc) && map[nr][nc] && map[r][c]) {
-				
-				map[r][c] = findRoot(nr, nc); // true가 리턴됐다면 이번 delta로 정한 다음 길은 사용 불가 && false가 리턴됐다면 파이프 설치 완료
-				
+			if(map[nr][nc] == '.') 	{
+				if(findRoot(nr, nc)) {
+					map[r][c] = 'x';
+					return true;
+				}
 			}
 		}
 		
-		if(map[r][c]) {
-			// 아무 길도 가지 못했다면 이 길 폐쇄 - 다시 오지 않기 위함
-			map[r][c] = false;
-			return true;
-		}
-		
+		map[r][c] = 'x';
 		return false;
 		
 	}
@@ -46,22 +38,18 @@ public class Main {
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		
-		map = new boolean[R][C];
+		map = new char[R+2][C+2];
 		
-		for(int i=0; i<R; i++) {
+		for(int i=1; i<=R; i++) {
 			char[] temp = br.readLine().toCharArray();
-			for(int j=0; j<C; j++) {
-				if(temp[j] == '.') {
-					map[i][j] = true; // 이 길로 갈 수 있음
-				} else {
-					map[i][j] = false; // 이 길로 갈 수 없음
-				}
+			for(int j=1; j<=C; j++) {
+				map[i][j] = temp[j-1];
 			}
 			
 		}
 		
-		for(int r=0; r<R; r++) {
-			findRoot(r, 0);
+		for(int r=1; r<=R; r++) {
+			findRoot(r, 1);
 		}
 		
 		System.out.println(ans);
