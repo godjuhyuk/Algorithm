75c17bb7b4d3c73b949faf8f5b80d8b2568888b1
godjuhyuk <59328108+godjuhyuk@users.noreply.github.com>
2023-08-22 14:34:57 +0900

    [Gold V] Title: 적록색약, Time: 156 ms, Memory: 17156 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,111 +1,75 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-	static int n;
-	static char[][] map;
-	static int[][] deltas = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-	static int[] colorCnt = new int[3];
-	static int[] colorCnt2 = new int[2];
 	
-	static boolean[][] visited;
-	static boolean[][] visited2;
+	private static int N, ans1, ans2;
+	private static int[][] deltas = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+	private static char[][] map;
+	private static boolean[][] visited;
 	
-	public static boolean isRedOrGreen(char color) {
-		if(color == 'R' || color == 'G') {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public static void ansCnt2(char color) {
-		if(color == 'R' || color == 'G') {
-			colorCnt2[0]++;
-		}
-		else if(color == 'B') {
-			colorCnt2[1]++;
-		}
+	private static void dfs(int r, int c, char color) {
 		
-	}
-	
-	public static void ansCnt(char color) {
-		if(color == 'R') {
-			colorCnt[0]++;
-		}
-		else if(color == 'G') {
-			colorCnt[1]++;
-		}
-		else if(color == 'B') {
-			colorCnt[2]++;
-		}
+		// 적록색약을 위한 mapChange
+		if(color == 'G') map[r][c] = 'R';
 		
-	}
-	
-	public static void dfs2(int r, int c, char color){
-		for(int[] delta: deltas) {
-			int nr = r + delta[0];
-			int nc = c + delta[1];
-			char nextColor = map[nr][nc]; 
-			if(!visited2[nr][nc] && isRedOrGreen(color) && isRedOrGreen(nextColor)) {
-				visited2[nr][nc] = true;
-				dfs2(nr, nc, nextColor);
-			}
+		visited[r][c] = true;
+		
+		
+		for(int[] d: deltas) {
+			int nr = r + d[0];
+			int nc = c + d[1];
 			
-			// blue일떄
-			else if(!visited2[nr][nc] && nextColor == color){
-				visited2[nr][nc] = true;
-				dfs2(nr, nc, nextColor);
-			}
-		}
-	}
-	
-	public static void dfs(int r, int c, char color){
-		for(int[] delta: deltas) {
-			int nr = r + delta[0];
-			int nc = c + delta[1];
-			if(!visited[nr][nc] && map[nr][nc] == color) {
-				visited[nr][nc] = true;
+			if(map[nr][nc] == color && !visited[nr][nc]) {
 				dfs(nr, nc, color);
 			}
 		}
+		
+
 	}
 	
-	
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		map = new char[n+2][n+2];
-		visited = new boolean[n+2][n+2];
-		visited2 = new boolean[n+2][n+2];
-		for(int i=1; i<n+1; i++) {
+		N = Integer.parseInt(br.readLine());
+		
+		map = new char[N+2][N+2];
+		visited = new boolean[N+2][N+2];
+		
+		for(int i=1; i<=N; i++) {
 			char[] input = br.readLine().toCharArray();
-			for(int j=1; j<n+1; j++) {
+			for (int j=1; j<=N; j++) {
 				map[i][j] = input[j-1];
 			}
 		}
 		
-		for(int i=1; i<n+1; i++) {
-			for(int j=1; j<n+1; j++) {
-				
-				char color = map[i][j];
-
-				//적록색약 X
+		for(int i=1; i<=N; i++) {
+			for (int j=1; j<=N; j++) {
 				if(!visited[i][j]) {
-					visited[i][j] = true;
-					dfs(i, j, color);
-					ansCnt(color);
-				}
-				
-				//적록색약 O
-				if(!visited2[i][j]) {
-					visited2[i][j] = true;
-					dfs2(i, j, color);
-					ansCnt2(color);
+					//비적록색약 dfs
+					dfs(i, j, map[i][j]);
+					ans1++;
 				}
 			}
 		}
 		
-		System.out.print(colorCnt[0]+ colorCnt[1] + colorCnt[2] + " ");
-		System.out.print(colorCnt2[0]+ colorCnt2[1]);
+		visited = new boolean[N+2][N+2];
+		
+		
+		for(int i=1; i<=N; i++) {
+			for (int j=1; j<=N; j++) {
+				if(!visited[i][j]) {
+					//적록색약 dfs
+					dfs(i, j, map[i][j]);
+					ans2++;
+				}
+			}
+		}
+		
+		
+		System.out.println(ans1 + " " + ans2);
+		
 	}
+
 }
