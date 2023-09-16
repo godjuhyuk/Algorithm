a01ac60c352d17c781f3fe220420c61d7d81c4cc
godjuhyuk <59328108+godjuhyuk@users.noreply.github.com>
2023-09-16 13:17:14 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,65 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	
-	private static int N, map[][], ans;
-	
-	private static boolean isOutOfRange(int r, int c) {
-		return 0>r || r>=N || 0 > c || c>=N;
-	}
-	
-	private static boolean canDiagMove(int hr, int hc) {
-		
-		return !isOutOfRange(hr+1, hc+1) && map[hr+1][hc] == 0 && map[hr][hc+1] == 0 && map[hr+1][hc+1] == 0; 
-		
-	}
-	
-	private static void backtracking(int hr, int hc, int tr, int tc) {
-		if(hr==N-1 && hc == N-1) {
-			ans++;
-			return;
-		}
-		
-		// 가로 상태
-		if(hr==tr) {
-			if(!isOutOfRange(hr,hc+1) && map[hr][hc+1] == 0) backtracking(hr, hc+1, hr, hc);
-			if(canDiagMove(hr, hc) && map[hr][hc+1] == 0) backtracking(hr+1, hc+1, hr, hc);
-		}
-		
-		// 세로 상태
-		else if(hc==tc) {
-			if(!isOutOfRange(hr+1,hc) && map[hr+1][hc] == 0) backtracking(hr+1, hc, hr, hc);
-			if(canDiagMove(hr, hc)) backtracking(hr+1, hc+1, hr, hc);
-		}
-		
-		// 대각선 상태
-		else {
-			if(!isOutOfRange(hr+1,hc) && map[hr+1][hc] == 0) backtracking(hr+1, hc, hr, hc);
-			if(!isOutOfRange(hr,hc+1) && map[hr][hc+1] == 0) backtracking(hr, hc+1, hr, hc);
-			if(canDiagMove(hr, hc)) backtracking(hr+1, hc+1, hr, hc);
-		}
-		
-	}
-	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][][] dp = new int[N][N][3];
+		
 		for(int i=0; i<N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int j=0; j<N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+				if(Integer.parseInt(st.nextToken()) == 1) {
+					dp[i][j][0] = -1;
+					dp[i][j][1] = -1;
+					dp[i][j][2] = -1;
+				}
+				
+			}
+		}
+		if(dp[N-1][N-1][0] < 0 ) {
+            System.out.println(0);
+            return;
+        }
+        
+		// 초기값 세팅
+		dp[0][1][0] = 1;
+		
+		for(int i=0; i<N; i++) {
+			for(int j=2; j<N; j++) {
+				
+				// 벽인 경우 
+				if(dp[i][j][0] == -1) continue;
+				
+				// 가로 모양으로 도착한 경우
+				if(dp[i][j-1][0] != -1) dp[i][j][0] += dp[i][j-1][0] + dp[i][j-1][2];
+				
+				
+				if(i == 0) continue;
+				
+				// 세로 모양으로 도착한 경우
+				if(dp[i-1][j][0] != -1) dp[i][j][1] += dp[i-1][j][1] + dp[i-1][j][2];
+				
+				// 대각선 모양으로 도착한 경우
+				if(dp[i-1][j][0] != -1 && dp[i][j-1][0] != -1 && dp[i-1][j-1][0] != -1) {
+					dp[i][j][2] += dp[i-1][j-1][0] + dp[i-1][j-1][1] + dp[i-1][j-1][2];
+				}
+					
 			}
 		}
 		
-		
-		backtracking(0, 1, 0, 0);
-		
-		System.out.println(ans);
+		System.out.println(dp[N-1][N-1][0] + dp[N-1][N-1][1] + dp[N-1][N-1][2]);
 		
 	}
 	
