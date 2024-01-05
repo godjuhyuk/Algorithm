93dd64ea83468cb49c13dfc3fe6fc6ec386f87ee
godjuhyuk <59328108+godjuhyuk@users.noreply.github.com>
2024-01-05 16:47:33 +0900

    [Silver I] Title: 후보 추천하기, Time: 88 ms, Memory: 12064 KB -BaekjoonHub

---
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
@@ -0,0 +1,76 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	private static int N, M;
+	
+	private static class Student implements Comparable<Student> {
+		
+		int num;
+		int like;
+		int turn;
+		boolean isInFrame = false;
+		
+		public Student(int num, int like, int turn){
+			this.num = num;
+			this.like = like;
+			this.turn = turn;
+		}
+		
+		@Override
+		public int compareTo(Student o){
+			if(this.like == o.like){
+				return o.turn - this.turn;
+			}
+			return o.like - this.like;
+		}
+		
+	}
+	
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Student[] school = new Student[101];
+		List<Student> frame = new ArrayList<Student>();
+		
+		for(int i=0; i<M; i++) {
+			int recommendNum = Integer.parseInt(st.nextToken());
+			if(school[recommendNum] == null) {
+				school[recommendNum] = new Student(recommendNum, 1, i);
+			} 
+			
+			if(school[recommendNum].isInFrame){
+				school[recommendNum].like++;
+			} 
+			else {
+				school[recommendNum].turn = i;
+				if(frame.size() == N) {
+					Collections.sort(frame);
+					Student pollStd = frame.get(N-1);
+					frame.remove(N-1);
+					school[pollStd.num].like = 0;
+					school[pollStd.num].isInFrame = false;
+				}
+				frame.add(school[recommendNum]);
+				school[recommendNum].like = 1;
+				school[recommendNum].isInFrame = true;
+			}
+		}
+		
+		for(Student std : school) {
+			if(std != null && std.isInFrame){
+				System.out.print(std.num + " ");
+			}
+		}
+		
+	}
+}
