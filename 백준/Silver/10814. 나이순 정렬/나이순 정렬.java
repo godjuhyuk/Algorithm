9d80cc04b7a31873a18877d9a18baa4e4413fc6e
godjuhyuk <wngur4300@gmail.com>
2024-10-02 13:55:11 +0900

    [Silver V] Title: 나이순 정렬, Time: 464 ms, Memory: 46752 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,53 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int N;
-    private static Member[] members;
-    static class Member implements Comparable<Member> {
 
-        int num;
-        int age;
-        String name;
-
-        public Member(int num, int age, String name) {
-            this.num = num;
-            this.age = age;
-            this.name = name;
-        }
-
-        @Override
-        public int compareTo(Member om){
-            if(this.age == om.age) {
-                return Integer.compare(this.num, om.num);
-            }
-            return Integer.compare(this.age, om.age);
-        }
-
-    }
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        members = new Member[N];
-
-        for(int i=0; i<N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int age = Integer.parseInt(st.nextToken());
-            String name = st.nextToken();
-            members[i] = new Member(i, age, name);
-        }
-
-        Arrays.sort(members);
-        StringBuilder sb = new StringBuilder();
-        for(Member member: members) {
-            sb.append(member.age).append(' ').append(member.name).append('\n');
-        }
-        System.out.println(sb);
-    }
-
+	private static class UserInfo implements Comparable<UserInfo> {
+		
+		int age;
+		int order;
+		String name;
+		
+		public UserInfo(int age, int order, String name) {
+			super();
+			this.age = age;
+			this.order = order;
+			this.name = name;
+		}
+		
+		public int compareTo(UserInfo o) {
+			if(this.age == o.age) return this.order - o.order;
+			return this.age - o.age;
+		}
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		List<UserInfo> list = new ArrayList<UserInfo>();
+		
+		for(int i=0; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			list.add(new UserInfo(Integer.parseInt(st.nextToken()), i, st.nextToken()));
+		}
+		
+		Collections.sort(list);
+		StringBuilder sb = new StringBuilder();
+		for(UserInfo userInfo : list) sb.append(userInfo.age).append(' ').append(userInfo.name).append('\n');
+		
+		System.out.println(sb);
+	}
+	
 }
